0cb9f69b28974a36c946f3bf59bace93a6da4249
Albert Darchiev <albert.darchiev.7e6@itb.cat>
2024-02-22 15:22:26 +0100

    main

--- database.py
+++ database.py
@@ -6,8 +6,6 @@ from pydantic import BaseModel
 from typing import Optional, List
 
 URL_DATABASE = "postgresql://fl0user:bIqOB7Q5XrTi@ep-wild-snow-33454215.eu-central-1.aws.neon.fl0.io:5432/nomadworld?sslmode=require"
-#"postgresql://fl0user:Kj7obcqEh3LZ@ep-holy-breeze-83855958.eu-central-1.aws.neon.fl0.io:5432/database?sslmode=require"
-#"postgresql://fl0user:bIqOB7Q5XrTi@ep-wild-snow-33454215.eu-central-1.aws.neon.fl0.io:5432/nomadworld?sslmode=require"
 engine = create_engine(URL_DATABASE)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)  
